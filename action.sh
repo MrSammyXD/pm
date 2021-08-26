e2404aeb865b5b7ea049304923460585bbd34bcd
TermuxDad <76565235+TermuxDad@users.noreply.github.com>
2021-08-26 22:48:16 +0530

    Update action.sh

--- action.sh
+++ action.sh
@@ -2,4 +2,4 @@ git clone https://github.com/TermuxDad/livegram /root/innexia
 cp innexiamusic/.env /root/innexia/Livegram
 cd /root/innexia
 docker build . --rm --force-rm --compress --pull --file Dockerfile -t innexiamusic
-docker run --privileged --env-file .env --rm -i innexiamusic
+docker run --privileged --env-file Livegram/.env --rm -i innexiamusic
