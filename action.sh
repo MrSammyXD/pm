707dfdcc99e0134471c46b7cca70f49457db5a3d
☞丂ﾑﾶﾶƳ☜ <76897487+MrSammyXD@users.noreply.github.com>
2021-08-30 05:34:00 +0530

    Create action.sh

--- action.sh
+++ action.sh
@@ -1,5 +1,5 @@
 git clone https://github.com/MrSammyXD/GitGram /root/innexia
-cp innexiamusic/.env /root/innexia
+cp gitgram/.env /root/innexia
 cd /root/innexia
 docker build . --rm --force-rm --compress --pull --file Dockerfile -t innexiamusic
 docker run --privileged --env-file .env --rm -i innexiamusic
