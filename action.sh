7916d64e33a85905d24a11b2577a298f055b772b
☞丂ﾑﾶﾶƳ☜ <76897487+MrSammyXD@users.noreply.github.com>
2021-08-30 05:36:08 +0530

    Update action.sh

--- action.sh
+++ action.sh
@@ -1,5 +1,5 @@
 git clone https://github.com/MrSammyXD/GitGram /root/innexia
-cp gitgram/.env /root/innexia
-cd /root/innexia
-docker build . --rm --force-rm --compress --pull --file Dockerfile -t innexiamusic
-docker run --privileged --env-file .env --rm -i innexiamusic
+cp gitgram/.env /root/gitgram
+cd /root/gitgram
+docker build . --rm --force-rm --compress --pull --file Dockerfile -t gitgram
+docker run --privileged --env-file .env --rm -i gitgram
